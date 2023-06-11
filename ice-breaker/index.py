b6f67072b18c9c90d5f1a35bb2607afc2983b43d
xin.jin <jinxin479@gmail.com>
2023-06-11 11:57:20 +0800

    feat:add langchain note02

--- ice-breaker/index.py
+++ ice-breaker/index.py
@@ -1,30 +1,56 @@
-from langchain import PromptTemplate
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import LLMChain
-
-
+from typing import Tuple
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
+from chains.custom_chains import (
+    get_summary_chain,
+    get_interests_chain,
+    get_ice_breaker_chain,
+)
 from linkedin import scrape_linkedin_profile
 
+from twitter import scrape_user_tweets
+from output_parsers import (
+    summary_parser,
+    topics_of_interest_parser,
+    ice_breaker_parser,
+    Summary,
+    IceBreaker,
+    TopicOfInterest,
+)
 
-if __name__ == '__main__':
-    print("Hello Langchain")
-    linkedin_profile_url = linkedin_lookup_agent(name="siven.jin")
 
-    summary_template = """
-         given the Linkedin information {information} about a person from I want you to create:
-         1. a short summary
-         2. two interesting facts about them
-     """
-    summary_prompt_template = PromptTemplate(
-        input_variables=["information"],
-        template=summary_template
+def ice_break_with(name: str) -> Tuple[Summary, IceBreaker, TopicOfInterest, str]:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url=linkedin_username)
+
+    twitter_username = twitter_lookup_agent(name=name)
+    tweets = scrape_user_tweets(username=twitter_username)
+
+    summary_chain = get_summary_chain()
+    summary_and_facts = summary_chain.run(
+        information=linkedin_data, twitter_posts=tweets
+    )
+    summary_and_facts = summary_parser.parse(summary_and_facts)
+
+    interests_chain = get_interests_chain()
+    interests = interests_chain.run(
+        information=linkedin_data, twitter_posts=tweets)
+    interests = topics_of_interest_parser.parse(interests)
+
+    ice_breaker_chain = get_ice_breaker_chain()
+    ice_breakers = ice_breaker_chain.run(
+        information=linkedin_data, twitter_posts=tweets
+    )
+    ice_breakers = ice_breaker_parser.parse(ice_breakers)
+
+    return (
+        summary_and_facts,
+        interests,
+        ice_breakers,
+        linkedin_data.get("profile_pic_url"),
     )
 
-    llm = ChatOpenAI(temperature=0,model_name="gpt-3.5-turbo")
 
-    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
-    linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url=linkedin_profile_url)
-    
-    print(chain.run(information=linkedin_data))
+if __name__ == "__main__":
+    pass
