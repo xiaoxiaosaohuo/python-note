b464f6fd92d31a47a4b0f127c40d914abca99560
xin.jin <jinxin479@gmail.com>
2023-07-05 00:22:09 +0800

    feat:curd

--- django/base/urls.py
+++ django/base/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
+    path('create-form', views.createRoom, name="create-form"),
+    path('update-form/<str:pk>/', views.updateRoom, name="update-form"),
 ]
--- django/base/views.py
+++ django/base/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 # Create your views here.
 from .models import Room
+from .form import RoomForm
+
 
 # define route
 
@@ -17,3 +19,29 @@ def room(request,pk):
    
     context = {'room':room}
     return render(request, 'base/room.html',context)
+
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+       form = RoomForm(request.POST)
+       if form.is_valid():
+           form.save()
+           return redirect('home')
+   
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom (request,pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
