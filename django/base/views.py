7855a1bc87adf0749f227b9cd5343cce44b875cf
xin.jin <jinxin479@gmail.com>
2023-07-04 23:50:48 +0800

    feat:add models

--- django/base/views.py
+++ django/base/views.py
@@ -1,26 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from .models import Room
 
-rooms = [
-    {'id': 1, 'name': 'Lets learn python'},
-    {'id': 2, 'name': 'Design with me'},
-    {'id': 3, 'name': 'Frontend developers'},
-    {'id': 4, 'name': 'Backend developers'},
-    {'id': 5, 'name': 'Fullstack developers'},
-   
-]
 # define route
 
 
 def home(request):
+    rooms = Room.objects.all()
     return render(request, 'base/home.html',{'rooms':rooms})
 
 
 def room(request,pk):
     room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
+   
     context = {'room':room}
     return render(request, 'base/room.html',context)
