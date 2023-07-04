f0675731887904ce9289bb2b207334b63d96d642
xin.jin <jinxin479@gmail.com>
2023-07-04 08:38:26 +0800

    feat:add templates

--- django/base/views.py
+++ django/base/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+# Create your views here.
 
+rooms = [
+    {'id': 1, 'name': 'Lets learn python'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend developers'},
+    {'id': 4, 'name': 'Backend developers'},
+    {'id': 5, 'name': 'Fullstack developers'},
+   
+]
 # define route
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'base/home.html',{'rooms':rooms})
 
 
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'base/room.html')
