6fae8ddedb46ad9e07afce915e61776e4f537680
xin.jin <jinxin479@gmail.com>
2023-07-04 09:02:35 +0800

    feat:add templates

--- django/base/views.py
+++ django/base/views.py
@@ -17,5 +17,10 @@ def home(request):
     return render(request, 'base/home.html',{'rooms':rooms})
 
 
-def room(request):
-    return render(request, 'base/room.html')
+def room(request,pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room':room}
+    return render(request, 'base/room.html',context)
