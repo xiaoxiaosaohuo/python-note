fa0259ecf130d17ae9a66a8a46c1e56a9cff625c
xiaoxiaosaohuo <jinxin479@126.com>
2023-07-05 16:25:59 +0800

    feat:add new file

--- file.py
+++ file.py
@@ -1,3 +1,6 @@
+import requests
+import json
+import time
 # r open a file for reading
 # w open a file for writing
 # x open a file for exclusive creation, if the file already exists, the operation fails
@@ -58,3 +61,87 @@ print(odd())
 # call the outer function again
 odd2 = calculate()
 print(odd2())
+
+
+def openfile():
+    try:
+        with open('致橡树.txt', 'r', encoding='utf-8') as f:
+            print(f.read())
+    except FileNotFoundError:
+        print('无法打开指定的文件!')
+    except LookupError:
+        print('指定了未知的编码!')
+    except UnicodeDecodeError:
+        print('读取文件时解码错误!')
+
+
+def openfile():
+    # 一次性读取整个文件内容
+    with open('致橡树.txt', 'r', encoding='utf-8') as f:
+        print(f.read())
+
+    # 通过for-in循环逐行读取
+    with open('致橡树.txt', mode='r') as f:
+        for line in f:
+            print(line, end='')
+            time.sleep(0.5)
+    print()
+
+    # 读取文件按行读取到列表中
+    with open('致橡树.txt') as f:
+        lines = f.readlines()
+    print(lines)
+
+## 读写二进制文件
+
+
+def binaryfile():
+    try:
+        with open('1.jpg', 'rb') as fs1:
+            data = fs1.read()
+            print(type(data))  # <class 'bytes'>
+        with open('2.jpg', 'wb') as fs2:
+            fs2.write(data)
+    except FileNotFoundError as e:
+        print('指定的文件无法打开.')
+    except IOError as e:
+        print('读写文件时出现错误.')
+    print('程序执行结束.')
+
+
+## JSON
+
+
+def json():
+    mydict = {
+        'name': 'deny',
+        'age': 38,
+        'qq': 957658,
+        'friends': ['nick', 'maya'],
+        'cars': [
+            {'brand': 'BYD', 'max_speed': 180},
+            {'brand': 'Audi', 'max_speed': 280},
+            {'brand': 'Benz', 'max_speed': 320}
+        ]
+    }
+    try:
+        with open('data.json', 'w', encoding='utf-8') as fs:
+            json.dump(mydict, fs)
+    except IOError as e:
+        print(e)
+    print('保存数据完成!')
+
+
+# json模块主要有四个比较重要的函数，分别是：
+
+# dump - 将Python对象按照JSON格式序列化到文件中
+# dumps - 将Python对象处理成JSON格式的字符串
+# load - 将文件中的JSON数据反序列化成对象
+# loads - 将字符串的内容反序列化成Python对象
+
+
+def fetchData():
+    resp = requests.get('http://api.tianapi.com/guonei/?key=APIKey&num=10')
+    data_model = json.loads(resp.text)
+    for news in data_model['newslist']:
+        print(news['title'])
